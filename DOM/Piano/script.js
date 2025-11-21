7d19bb61527fa35031a3d007efae69b653714333
Ankur Kumar <ankur.kumar.adtu@gmail.com>
2025-11-22 00:20:31 +0530

    Piano done

---
+++ DOM/Piano/script.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("keydown", function (e) {
+
+    let q
+    let key
+    try {
+        let pressed = e.key.toLowerCase()
+        q = new Audio(`./audio/${pressed}.mp3`)
+        key = document.querySelector(`.${pressed}`)
+    } catch (error) {
+        console.log("Invalid key")
+    }
+    key.style.backgroundColor = "lightblue";
+    key.style.transform = "scale(0.8)";
+
+    setTimeout(() => {
+        key.style.backgroundColor = "white";
+        key.style.transform = "scale(1)";
+    }, 100);
+    q.play()
+})
