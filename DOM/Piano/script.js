3c265af6d40fe23f746413c261a027b668195d44
Ankur Kumar <ankur.kumar.adtu@gmail.com>
2025-11-22 00:46:45 +0530

    test

--- DOM/Piano/script.js
+++ DOM/Piano/script.js
@@ -10,7 +10,7 @@ document.addEventListener("keydown", function (e) {
     } catch (error) {
         console.log("Invalid key")
     }
-    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
+    
     key.style.backgroundColor = "lightblue";
     key.style.transform = "scale(0.7)";
 
